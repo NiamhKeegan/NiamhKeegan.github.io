30e343883f6763162c72500111467c9947936793
NiamhKeegan <126117590+NiamhKeegan@users.noreply.github.com>
2023-04-17 19:07:38 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function go(){
-	var name=document.getElementById("name").value;
-	var email=document.getElementById("email").value;
-	document.getElementById("hide").style.display = "none";	
-	document.getElementById("span1").innerHTML = name;
-	document.getElementById("span2").innerHTML = email;
-	document.getElementById("show").style.display = "block";	
-}
